766fdabfb8356c2b3f7558480ed67ad21ef47589
Shreyas Kumar <shreyaskumar467@gmail.com>
2023-10-25 00:52:30 +0530

    new apk release

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.simplecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.simplecalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnplus.setOnClickListener {
+            val input1=binding.input1.text.toString().toInt()
+            val input2=binding.input2.text.toString().toInt()
+            binding.textView4.text=(input1+input2).toString()
+
+        }
+        binding.btnminus.setOnClickListener {
+            val input1=binding.input1.text.toString().toInt()
+            val input2=binding.input2.text.toString().toInt()
+            binding.textView4.text=(input1-input2).toString()
+
+        }
+        binding.btnmultiply.setOnClickListener {
+            val input1=binding.input1.text.toString().toInt()
+            val input2=binding.input2.text.toString().toInt()
+            binding.textView4.text=(input1*input2).toString()
+
+        }
+        binding.btndivide.setOnClickListener {
+            val input1=binding.input1.text.toString().toInt()
+            val input2=binding.input2.text.toString().toInt()
+            binding.textView4.text=(input1/input2).toString()
+
+        }
+
+
+
+
+    }
+}
